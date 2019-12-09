f36c8a9d450fff06ac830205a3264863846abcda
ToninoTarsi <tony.tarsi@gmail.com>
2019-12-09 15:52:11 +0100

    Added airspace to url parser

--- js/app/main.js
+++ js/app/main.js
@@ -4,31 +4,32 @@
  */
 define(['app/map', 'app/uploader', 'jquery'], function(map, uploader, $) {
   // Grab param from url...
-  // Waypoints Url
-  var waypointsUrl = location.search.split('waypointsUrl=')[1];
-  // Task Url
-  var taskUrl = location.search.split('taskUrl=')[1];
-  // Task Id
-  var taskId = location.search.split('taskId=')[1]
-  // Hide sidebar.
-  if (location.search.split('hideSidebar=true')[1]) {
-    $("#sidebar").hide();
-    $("#map-canvas").css({
-      width: '100%'
-    });
-  }
 
-  if (waypointsUrl) {
-    var filename = waypointsUrl.split("/").pop();
-    $.get(waypointsUrl, function(data) {
+  let params = getJsonFromUrl(location.href);
+
+  
+  //load_params(params.airspaceUrl,params.waypointsUrl);
+
+  if (params.waypointsUrl) {
+    var filename = params.waypointsUrl.split("/").pop();
+    $.get(params.waypointsUrl, function(data) {
       uploader.setFilename(filename);
       uploader.parse(data);
-    });
+    } , "text");
   }
 
-  if (taskUrl) {
+
+  if (params.airspaceUrl) {
+    //var filename = params.airspaceUrl.split("/").pop();
+    $.get(params.airspaceUrl, function(data) {
+      uploader.setFilename("Airspace.kml");
+      uploader.parse(data);
+    } , "text");
+  }
+
+  if (params.taskUrl) {
     var filename = taskUrl.split("/").pop();
-    $.get(taskUrl, function(data) {
+    $.get(params.taskUrl, function(data) {
       uploader.setFilename(filename);
       if (typeof(data) !== "string") {
         data = new XMLSerializer().serializeToString(data);
@@ -37,8 +38,8 @@ define(['app/map', 'app/uploader', 'jquery'], function(map, uploader, $) {
     });
   }
 
-  if (taskId) { 
-    var id = taskId.split("/").pop();
+  if (params.taskId) { 
+    var id = params.taskId.split("/").pop();
     $.get("https://pole.ffvl.fr/tasks/" + id, function(data) {
      var geo = data[0].data;
      uploader.setFilename( id + '.geojson');
@@ -46,8 +47,62 @@ define(['app/map', 'app/uploader', 'jquery'], function(map, uploader, $) {
     });
   }
   
-  /** @todo
-   * Enable variable that directly display a given task
-   */
+
+
+  // async function load_params(airspaceUrl,waypointsUrl) {
+
+
+  //   if (waypointsUrl) {
+  //     var filename = params.waypointsUrl.split("/").pop();
+  //     $.get(waypointsUrl, function(data) {
+  //       uploader.setFilename(filename);
+  //       uploader.parse(data);
+  //     } , "text");
+  //   }
   
+  //   await sleep(2000);
+
+  //   if (airspaceUrl) {
+  //     //var filename = params.airspaceUrl.split("/").pop();
+  //     $.get(airspaceUrl, function(data) {
+  //       uploader.setFilename("Airspace.kml");
+  //       uploader.parse(data);
+  //     } , "text");
+  //   }
+
+  // }
+
+  // function sleep(ms) {
+  //   return new Promise(resolve => setTimeout(resolve, ms));
+  // }
+  
+  function getJsonFromUrl(url) {
+    if(!url) url = location.href;
+    var question = url.indexOf("?");
+    var hash = url.indexOf("#");
+    if(hash==-1 && question==-1) return {};
+    if(hash==-1) hash = url.length;
+    var query = question==-1 || hash==question+1 ? url.substring(hash) : 
+    url.substring(question+1,hash);
+    var result = {};
+    query.split("&").forEach(function(part) {
+      if(!part) return;
+      part = part.split("+").join(" "); // replace every + with space, regexp-free version
+      var eq = part.indexOf("=");
+      var key = eq>-1 ? part.substr(0,eq) : part;
+      var val = eq>-1 ? decodeURIComponent(part.substr(eq+1)) : "";
+      var from = key.indexOf("[");
+      if(from==-1) result[decodeURIComponent(key)] = val;
+      else {
+        var to = key.indexOf("]",from);
+        var index = decodeURIComponent(key.substring(from+1,to));
+        key = decodeURIComponent(key.substring(0,from));
+        if(!result[key]) result[key] = [];
+        if(!index) result[key].push(val);
+        else result[key][index] = val;
+      }
+    });
+    return result;
+  }
+
 });
